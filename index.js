e9c4296290b5019907822b34c836bec712475d31
anaivp18 <avillpas12@gmail.com>
2019-03-20 10:22:59 +0100

    Base de datos de nombres creada

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ const puerto=3000;
 app.get("/",function(req,res){
 res.sendFile(__dirname+'/index.html');
 });
+app.post("/",function(req,res){
+  //Recojo los valores que envian desde el formulario
+  console.log(req.body);
+  var sexo=(req.body.sexo);
+  var apellidoPaterno=(req.body.apadre);
+  var apellidoMaterno=(req.body.amadre);
+});
+
 //Arranca el servidor
 app.listen(puerto,function(){
   console.log("El servidor está listo y escuchando en el puerto 3000");
