3164c1eae33f1aeef3fb25d2090dad854e00902b
anaivp18 <avillpas12@gmail.com>
2019-03-20 12:22:05 +0100

    Ya funcionando pero sin validar formulario y sin estilos

--- index.js
+++ index.js
@@ -23,6 +23,30 @@ app.post("/",function(req,res){
   var sexo=(req.body.sexo);
   var apellidoPaterno=(req.body.apadre);
   var apellidoMaterno=(req.body.amadre);
+  //Declaro dos Arrays de NOMBRES
+  var nombresNene=["Andrés","Julián","Pelayo","Martin","Rodrigo"];
+  var nombresNena=["Jimena","Lucia","Andrea","Patricia","Sandra"];
+
+  //Escogemos aleatoriamente unnombre
+  // 1. Genera un numero aleatorio entre 0 y X-1, siendo X
+  // el número de nombres total del que dispongo
+  // Ese número será la posición en el Array de nombres
+  // En mi caso sería entre 0 y 4
+var posicion=Math.round(Math.random()*4);
+console.log(posicion);
+
+//Comprobamos si es niño o niña y respondemos con un nombre
+res.writeHead(200, {"Content-Type": "text/html;charset=UTF-8"});
+res.write("<h1>Enhorabuena!!</h1>");
+if (sexo==="1"){
+  //Es niño
+  res.write("<h2>Puedes llamarle "+nombresNene[posicion]+" "+apellidoPaterno+" "+apellidoMaterno+"</h2>");
+  }
+if (sexo==="2"){
+  //Es niña
+  res.write("<h2>Puedes llamarle "+nombresNena[posicion]+" "+apellidoPaterno+" "+apellidoMaterno+"</h2>");
+  }
+res.send();
 });
 
 //Arranca el servidor
